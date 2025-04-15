4734da0a842eb880a8f510b888c9c3fa93e5d2ff
Ian Day <ian@ianday.me>
2025-04-14 22:07:13 -0400

    feat(Users): change id type to UUID

--- app/users/models.py
+++ app/users/models.py
@@ -1,3 +1,4 @@
+import uuid
 from django.db import models
 from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 
@@ -30,28 +31,29 @@ class MyCustomUserManager(BaseUserManager):
        return user
    
 class User(AbstractBaseUser):
-   email = models.EmailField(unique=True)
-   first_name = models.CharField(max_length=30, blank=True)
-   last_name = models.CharField(max_length=30, blank=True)
-   is_active = models.BooleanField(default=True)
-   is_admin = models.BooleanField(default=False)
-   timezone = models.CharField(max_length=30, default='UTC')
-   is_custom = models.BooleanField(default=False)
-   is_staff = models.BooleanField(default=False)
-   created_at = models.DateTimeField(auto_now_add=True)
-   updated_at = models.DateTimeField(auto_now=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,primary_key=True, editable=False)
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    timezone = models.CharField(max_length=30, default='UTC')
+    is_custom = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-   objects = MyCustomUserManager()
-   USERNAME_FIELD = 'email'
-   EMAIL_FIELD = 'email'
+    objects = MyCustomUserManager()
+    USERNAME_FIELD = 'email'
+    EMAIL_FIELD = 'email'
 
-   def __str__(self):
-       return self.email
-   def has_perm(self, perm, obj=None):
-       return True
-   def has_module_perms(self, app_label):
-       return True
+    def __str__(self):
+        return self.email
+    def has_perm(self, perm, obj=None):
+        return True
+    def has_module_perms(self, app_label):
+        return True
 
-   @property
-   def is_utc(self):
-       return self.timezone == 'UTC'
+    @property
+    def is_utc(self):
+        return self.timezone == 'UTC'
